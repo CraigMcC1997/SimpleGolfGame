92350db2ec1ea956e6dda9e568237653143a5a8d
Craig <craigmccorrisken@gmail.com>
2025-08-13 14:47:06 +0100

    Added a forth level Started working on the UI for the Menus and Scenes

--- Assets/Scripts/Debugger_manager.cs
+++ Assets/Scripts/Debugger_manager.cs
@@ -3,13 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class Debugger_manager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         DontDestroyOnLoad(this.gameObject);
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.anyKeyDown)
