b60eb0cab4c2d3c699d478f7f376c899efbfc3c1
Craig <craigmccorrisken@gmail.com>
2025-09-25 12:10:51 +0100

    1.0 - Centred hole in one text position - Moved text outside of gamemanager and into the GameUI object instead - Updated GameUI to allow for sections, etc top bar and HoleInOne sections

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,14 +14,13 @@ public class GameManager : MonoBehaviour
     ParticleSystem holeInOneParticlesInstance;
     float ballSpeed;
 
-    GameObject hole;
+    public GameObject holeinOneText;
 
     const int NUM_MENUS = 4; // Main Menu and Game Over
 
     void Start()
     {
-        hole = GameObject.Find("Hole in One");
-        hole.SetActive(false);
+        holeinOneText.SetActive(false);
     }
 
     void updateHighScore(int level)
@@ -65,9 +64,10 @@ public class GameManager : MonoBehaviour
     {
         if (BallManager.shots_left == 1)
         {
-            hole.SetActive(true);
-            //TMP disabled until I can progress more with partical systems
-            //holeInOneParticlesInstance = Instantiate(holeInOneParticles, hole.transform.position, Quaternion.identity);
+            holeinOneText.SetActive(true);
+
+            //!!TODO!! TMP disabled until I can progress more with partical systems
+            //holeInOneParticlesInstance = Instantiate(holeInOneParticles, holeinOneText.transform.position, Quaternion.identity);
         }
     }
 
