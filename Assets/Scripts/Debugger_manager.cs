af2c6015c5de42fabdcd75103369b50226707bd0
Craig <craigmccorrisken@gmail.com>
2025-11-09 14:15:39 +0000

    DEBUG OPTION: Added 2 new debug options - Reload current level - Press M to move ball to mouse current position

--- Assets/Scripts/Debugger_manager.cs
+++ Assets/Scripts/Debugger_manager.cs
@@ -22,12 +22,36 @@ public class Debugger_manager : MonoBehaviour
 
     void Update()
     {
+        // load the level corresponding to the number key pressed (1 = level 1, etc)
         if (Input.anyKeyDown)
         {
             int buildIndex = SceneUtility.GetBuildIndexByScenePath("Scenes/Level/" + Input.inputString);
             if (buildIndex != -1)
             {
                 SceneManager.LoadScene(buildIndex);
+            }
+        }
+
+        // Reload current level
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        // Move ball to mouses position when M is pressed
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0f; // Ensure z-coordinate is zero for 2D
+            GameObject ball = GameObject.FindGameObjectWithTag("Player");
+            if (ball != null)
+            {
+                ball.transform.position = mousePos;
+                Rigidbody2D ballRb = ball.GetComponent<Rigidbody2D>();
+                if (ballRb != null)
+                {
+                    ballRb.linearVelocity = Vector2.zero; // Stop ball movement   
+                }
             }
         }
     }
